872159f9e0f54da5f7666c7f0ac01128dc56f711
DiamondTran <tranvankimcuong@gmail.com>
2019-08-13 12:39:42 +0700

    update

--- app/src/main/java/com/diamond/diamond/thuchi/activity/NoteActivity.java
+++ app/src/main/java/com/diamond/diamond/thuchi/activity/NoteActivity.java
@@ -6,26 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.diamond.diamond.thuchi.R;
 
 public class NoteActivity extends AppCompatActivity {
-
+private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+webView= findViewById(R.id.webview);
+webView.setWebViewClient(new WebViewClient());
+webView.loadUrl("https://vietnamnet.vn/vn/kinh-doanh/thi-truong/");
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
--- app/src/main/java/com/diamond/diamond/thuchi/activity/ThongkeActivity.java
+++ app/src/main/java/com/diamond/diamond/thuchi/activity/ThongkeActivity.java
@@ -1,25 +1,121 @@
 package com.diamond.diamond.thuchi.activity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.diamond.diamond.thuchi.R;
+import com.diamond.diamond.thuchi.model.Chi;
+import com.diamond.diamond.thuchi.model.Thu;
+import com.diamond.diamond.thuchi.sqldao.ChiDAO;
+import com.diamond.diamond.thuchi.sqldao.ThuDAO;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ThongkeActivity extends AppCompatActivity {
-
+private static String TAG ="ThongkeActivity";
+private float[] ydata= {40.5f,59.5f};
+private String[] xdata= {"Thu","Chi"};
+PieChart pieChart;
+    int b, tongchi = 0,tong=0;
+    int a, tongthu = 0;
+    ArrayList<Integer> integers = new ArrayList<>();
+    ArrayList<Integer> integerss = new ArrayList<>();
+    private ThuDAO thuDao;
+    private ChiDAO chiDAO;
+    private List<Chi> chis;
+    private List<Thu> thus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thongke);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+    pieChart= findViewById(R.id.piechart);
+    pieChart.setRotationEnabled(true);
+    pieChart.setHoleRadius(25f);
+    pieChart.setTransparentCircleAlpha(0);
+    pieChart.setCenterText("Thu Chi");
+    pieChart.setCenterTextSize(10);
+//    pieChart.setDrawEntryLabels(true);
+      addDataSet();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void addDataSet() {
+        ArrayList<PieEntry> yEntry= new ArrayList<>();
+        ArrayList<String> xEntry= new ArrayList<>();
+
+        for (int i =0; i <ydata.length; i++){
+            yEntry.add(new PieEntry(ydata[i],i));
+        }
+        for (int i =0; i <xdata.length; i++){
+            xEntry.add(xdata[i]);
+        }
+
+        PieDataSet pieDataSet= new PieDataSet(yEntry, "Thu");
+        pieDataSet.setSliceSpace(2);
+        pieDataSet.setValueTextSize(12);
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        colors.add(Color.GRAY);
+        colors.add(Color.BLUE);
+
+        pieDataSet.setColors(colors);
+
+        Legend legend = pieChart.getLegend();
+        legend.setForm(Legend.LegendForm.CIRCLE);
+
+
+        PieData pieData= new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.invalidate();
+
+    }
+    private void tongthu() {
+        Thu thu;
+        thuDao = new ThuDAO(ThongkeActivity.this);
+        thus = thuDao.getALLThu();
+        for (int i = 0; i < thus.size(); i++) {
+            thu = thus.get(i);
+            a = Integer.parseInt(thu.date);
+            integers.add(a);
+        }
+
+        Chi chi;
+        chiDAO = new ChiDAO(ThongkeActivity.this);
+        chis= chiDAO.getALLChi();
+        for (int j =0; j<chis.size(); j ++){
+            chi = chis.get(j);
+            b = Integer.parseInt(chi.sotien);
+            integerss.add(b);
+        }
+        for (Integer element : integerss) {
+            tongchi += element;
+
+        }
+        for (Integer element : integers) {
+            tongthu += element;
+
+        }
+        tong = tongthu- tongchi;
     }
 
 }
